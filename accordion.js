7cf537244d28813a9b8801a4e16f69494b138c1e
OliaMedinska <medinska2@gmail.com>
2024-04-10 23:54:29 +0200

    init

--- accordion.js
+++
@@ -1,3 +0,0 @@
-const item = document.querySelectorAll(".features__accordion-item");
-const btn = document.querySelectorAll(".features__accordion-btn");
-const icon = document.querySelectorAll(".features__accordion-polygon");
